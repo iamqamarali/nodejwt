b0d9cd979b37adf63ea145a2fc4045a8db0d9568
Qamar Ali <iamqamarali1@gmail.com>
2023-02-06 20:21:42 +0500

    asd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const AuthMiddleware = require('./middleware/AuthMiddleware');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, function(){
     console.log('localhost:'+port)
 })
